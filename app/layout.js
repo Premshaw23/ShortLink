efdced6ccbfebfecc18492e602bb5875a319cccb
Premshaw23 <shawprem217@gmail.com>
2025-06-18 00:14:35 +0530

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <Toaster position="top-center" reverseOrder={false} />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
